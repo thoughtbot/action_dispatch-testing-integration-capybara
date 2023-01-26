6a596bb66b7bb06eb4c8804a0fe82abf09bf0d6a
Sean Doyle <sean.p.doyle24@gmail.com>
2023-01-26 16:19:06 -0500

    Reset Capybara's document between multiple requests
    When invoking multiple requests in sequence, clear whatever document
    contents the `Capybara::Session` has cached before issuing the request.
    
    If no request has been made, or if the intermediate response is never
    parsed, skip the cache clearing.

--- spec/action_dispatch/testing/integration/capybara/rspec_spec.rb
+++ spec/action_dispatch/testing/integration/capybara/rspec_spec.rb
@@ -10,6 +10,20 @@ RSpec.describe "Capybara extensions", type: :request do
     expect(page).to have_button "A button"
   end
 
+  it "clears page across multiple requests" do
+    post templates_path, params: { template: <<~HTML }
+      <button>Request 1</button>
+    HTML
+
+    expect(page).to have_button "Request 1"
+
+    post templates_path, params: { template: <<~HTML }
+      <button>Request 2</button>
+    HTML
+
+    expect(page).to have_button "Request 2"
+  end
+
   it "supports Capybara::Session#within" do
     post templates_path, params: { template: <<~HTML }
       <section><h1>Hello, World!</h1></section>
